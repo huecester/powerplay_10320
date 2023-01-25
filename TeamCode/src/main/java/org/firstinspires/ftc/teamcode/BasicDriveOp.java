875dd8d600b3f0de2bbdbd5b95cf5a996ac167ae
huecester <jericho.paulino@yahoo.com.ph>
2023-01-25 00:40:25 -0800

    Field-centric driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveOp.java
@@ -9,9 +9,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-@TeleOp(name = "Basic driving", group = "Driver")
+@TeleOp(name = "Basic driving (field-centric)", group = "Driver")
 public class BasicDriveOp extends LinearOpMode {
-	static final double STRAFING_MODIFIER = 1.1;
+	private static final double STRAFING_MODIFIER = 1.1;
+
+	private static final RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+	private static final RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
 
 	@Override
 	public void runOpMode() throws InterruptedException {
@@ -23,10 +26,11 @@ public class BasicDriveOp extends LinearOpMode {
 		DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
 		DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
 		DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+		IMU imu = hardwareMap.get(IMU.class, "imu");
 
 		telemetry.log().add("Configuring motors...");
-		frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-		backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+		frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 		frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
 		backRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
@@ -45,14 +49,31 @@ public class BasicDriveOp extends LinearOpMode {
 		frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 		backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+		telemetry.log().add("Initializing IMU...");
+		imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(logoDirection, usbDirection)));
+
 		telemetry.log().add("Initialized.");
 		waitForStart();
 
 		while (opModeIsActive()) {
-			// https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+			// TODO: add telemetry
+
+			if (gamepad1.y) {
+				imu.resetYaw();
+				telemetry.addData("Heading", "Reset.");
+			} else {
+				telemetry.addData("Heading", "Press (Y) to reset.");
+			}
+
+			// https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html#robot-centric-final-sample-code
 			double y = -gamepad1.left_stick_y;
 			double x = gamepad1.left_stick_x * STRAFING_MODIFIER; // Make strafing more powerful
 			double turn = gamepad1.right_stick_x;
+
+			// https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html#field-centric
+			double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+			x = x * Math.cos(heading) - y * Math.sin(heading);
+			y = x * Math.sin(heading) + y * Math.cos(heading);
 
 			// Clip power range to [0.0, 1.0]
 			double denominator = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(turn), 1);
@@ -60,6 +81,8 @@ public class BasicDriveOp extends LinearOpMode {
 			backLeft.setPower((y - x + turn) / denominator);
 			frontRight.setPower((y - x - turn) / denominator);
 			backRight.setPower((y + x - turn) / denominator);
+
+			telemetry.update();
 		}
 	}
 }
