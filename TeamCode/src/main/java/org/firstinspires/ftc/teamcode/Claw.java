6a07accd213a2c4fde75a825b123929f624d2754
huecester <jericho.paulino@yahoo.com.ph>
2023-01-27 15:40:03 -0800

    Initialize hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -1,11 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Claw {
+	private final ServoImplEx left;
+	private final ServoImplEx right;
+
 	public Claw(HardwareMap hardwareMap, Telemetry telemetry) {
-		// TODO: implement
+		// TODO: other things
+		telemetry.log().add("Setting up claw hardware...");
+
+		left = hardwareMap.get(ServoImplEx.class, "clawLeft");
+		right = hardwareMap.get(ServoImplEx.class, "clawRight");
+
+		telemetry.log().add("Claw is ready.");
 	}
+
+	// TODO: control claw
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slide.java
@@ -1,11 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Slide {
+	private final DcMotorEx motor;
+
 	public Slide(HardwareMap hardwareMap, Telemetry telemetry) {
-		// TODO: implement
+		// TODO: other things
+		telemetry.log().add("Setting up slide hardware...");
+		motor = hardwareMap.get(DcMotorEx.class, "slide");
+		telemetry.log().add("Slide is ready.");
 	}
+
+	// TODO: control slide
 }
