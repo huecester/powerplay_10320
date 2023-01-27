c0250c552268175e574dc56711556e2cb271b85f
huecester <jericho.paulino@yahoo.com.ph>
2023-01-27 15:27:01 -0800

    Add skeleton classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Claw {
+	public Claw(HardwareMap hardwareMap, Telemetry telemetry) {
+		// TODO: implement
+	}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slide.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Slide {
+	public Slide(HardwareMap hardwareMap, Telemetry telemetry) {
+		// TODO: implement
+	}
+}
