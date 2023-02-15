9c680d45775dfa4219c56eef5cf2094100c02808
huecester <jericho.paulino@yahoo.com.ph>
2023-02-14 17:23:03 -0800

    Road Runner tuning files

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/BasicOpmode.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class BasicOpmode extends LinearOpMode {
+	@Override
+	public void runOpMode() throws InterruptedException {
+		DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
+	}
+}
